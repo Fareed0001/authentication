8a3defab6e8e89a3c33e06443c7c737d32aa4730
Farid <fareedibrahim0003@gmail.com>
2023-02-12 12:11:00 +0100

    facebook authentication

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy; //this uses google OAuth and we will use it as a passport strategy
+const FacebookStrategy = require("passport-facebook"); //this uses facebook and we will use it as a passport strategy
+const findOrCreate = require("mongoose-findorcreate"); //for mongoose find or create one
 
 const app = express();
 
@@ -33,13 +36,16 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 });
 
 //creating encryption schema for the database https://www.npmjs.com/package/mongoose-encryption
-const userSchema = new mongoose.Schema ({ //This is to change our schema into a mongoose object schema ie https://www.npmjs.com/package/mongoose-encryption or https://mongoosejs.com/docs/schematypes.html
+const userSchema = new mongoose.Schema({ //This is to change our schema into a mongoose object schema ie https://www.npmjs.com/package/mongoose-encryption or https://mongoosejs.com/docs/schematypes.html
   email: String,
-  password: String
+  password: String,
+  googleId: String, //this takes their google id into the schema
+  facebookId: String //this takes their facebook id into the schema
 });
 
 //passport-local-mongoose
 userSchema.plugin(passportLocalMongoose); //this is what we will use to hash and salt our data and save it in our mongoose database
+userSchema.plugin(findOrCreate); //for mongoose fidorcreate
 
 //creating a model for the database
 const User = new mongoose.model("User", userSchema);
@@ -48,13 +54,96 @@ const User = new mongoose.model("User", userSchema);
 // use static authenticate method of model in LocalStrategy
 passport.use(User.createStrategy());
 
-// use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser()); //this will create a cookie to store user data
-passport.deserializeUser(User.deserializeUser()); //this will destroy the cookie to reveal the user information
+//this code is from the passport docs (https://www.passportjs.org/tutorials/google/session/)
+passport.serializeUser(function(user, cb) { //this creates a cookie
+  process.nextTick(function() {
+    cb(null, {
+      id: user.id,
+      username: user.username,
+      name: user.name
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) { //this destroys the cookie
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+//google strategy code from passport site "https://www.passportjs.org/packages/passport-google-oauth20/"
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID, //the client id stored in the .env file
+    clientSecret: process.env.CLIENT_SECRET, //the client secret code stored in the .env file
+    callbackURL: "http://localhost:3000/auth/google/secrets", //the Authorised redirect URIs you set in the google developer console (inside credentions, OAuth 2.0 Client IDs click the project name and scroll down)
+    // the path the callbackURL hits up in the server is /auth/google/secrets
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo" //this makes retrieving user data instead of from the deprecated google plus, it should use the userinfo
+    //be careful of typos
+  },
+  function(accessToken, refreshToken, profile, cb) { //here is where google sends back the access token that will allow us the user data
+    console.log(profile); //this is to log the user profile that we get back from the get request on /auth/google route
+    User.findOrCreate({ //we use the data we got back from google ie user email to find a user if they exist, if they dont to create one
+      username: profile.emails[0].value, //this adds it as a new mail so we can dodge an error
+      googleId: profile.id //when a new user gets created, this finds if the user googleId record already exists in our database, in which case we save all the data associated with that id otherwise we create on our database and save that information for the future
+    }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+//facebook strategy. code from https://www.passportjs.org/packages/passport-facebook/
+passport.use(new FacebookStrategy({ //this will create a new facebook strategy
+    clientID: process.env.FACEBOOK_APP_ID, //the clientID stored in the .env file
+    clientSecret: process.env.FACEBOOK_APP_SECRET, //the clientSecret stored in the .env file
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) { //here is where facebook sends back the access token that will allow us the user data
+    console.log(profile); //this is to log the user profile that we get back from the get request on auth/facebook route
+    User.findOrCreate({ //we use the data we got back from facebook ie username to find a user if they exist, if they dont to create one
+      facebookId: profile.id //when a new user gets created, this finds if the user facebook record already exists in our database, in which case we save all the data associated with that user otherwise we create on our database and save that information for the future
+    }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
   res.render("home");
   //this will render the home.ejs page as the starting page
+});
+
+//path for google button. this code was gotten from the passport-google-oauth20 docs (https://www.passportjs.org/packages/passport-google-oauth20/)
+app.get("/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile","email"]//this increases the scope to accept email so that we can bypass an error
+  }) //this will authenticate the user using google strategy. we are telling google that we need the users profile which includes their username and id
+  //the code creates a pop-up that allows users sign in into their google accounts
+  //it will initiate
+);
+
+//after user is authenticated using google, they are sent to this route
+app.get("/auth/google/secrets", //this is the route you provided in the google app console under credentials, OAuth 2.0 Client IDs, Authorised redirect URIs
+  passport.authenticate("google", {
+    failureRedirect: "/login"
+  }), //we authenticate the user locally and if there is any problem we send them back to the login page
+  function(req, res) {
+    res.redirect("/secrets"); //successful authentication and we send them to the secret route (to app.get /secrets)
+});
+
+//authenticating requests using facebook. code from (https://www.passportjs.org/packages/passport-facebook/)
+app.get("/auth/facebook",
+  passport.authenticate('facebook') //this will authenticate the user using facebook strategy. we are telling facebook that we need the users profile which includes their username and id
+  //the code creates a pop-up that allows users sign in into their facebook accounts
+);
+
+//after user is authenticated using facebook, they are sent to this route
+app.get("/auth/facebook/secrets",
+  passport.authenticate("facebook", {
+    failureRedirect: "/login"
+  }),//we authenticate the user locally and if there is any problem we send them back to the login page
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets"); //successful authentication and we send them to the secret route (to app.get /secrets)
 });
 
 app.get("/login", function(req, res) {
@@ -69,9 +158,9 @@ app.get("/register", function(req, res) {
 
 app.get("/secrets", function(req, res) {
   //here is where we check if the user is authenticated
-  if (req.isAuthenticated()){ //if the user is logged in then render the secrets page
+  if (req.isAuthenticated()) { //if the user is authenticated we render the secrets page
     res.render("secrets");
-  } else { //else send them to the login page so that they wii login
+  } else { //else send them to the login page so that they will login
     res.render("login");
   }
 });
@@ -89,20 +178,21 @@ app.get("/logout", function(req, res) { //here we deauthenticate the user and en
 
 //using passport to authenticate new users
 app.post("/register", function(req, res) { //This is to recieve the post request from the register form
-  User.register({username: req.body.username}, req.body.password, function(err, user) { //using the User model, the argument thats a username value, password and a function
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) { //using the User model, the argument thats a username value, password and a function
     if (err) {
       console.log(err);
       res.redirect("/register"); //this returns the user back to the regiser page so that they can retry
     } else { //if there are no errors we authenticate the user
       passport.authenticate("local")(req, res, function() { //this function only works if the authentication was successful
-          res.redirect("/secrets"); //when the authentication works, the user gets sent into the secrets route
+        res.redirect("/secrets"); //when the authentication works, the user gets sent into the secrets route
       });
     }
   });
 });
 
 app.post("/login", function(req, res) { //this route is to login after users have already registered. it i below the register route because you need to be inside the database before you can login
-
   const user = new User({ //we create a new user
     username: req.body.username,
     password: req.body.password
@@ -117,12 +207,7 @@ app.post("/login", function(req, res) { //this route is to login after users hav
       });
     }
   });
-
 });
-
-
-
-
 
 
 
